81ca975a4d348b2e7dc2d47078daaa4a0d217699
mesutKoc <mkoc1367@gmail.com>
2023-10-06 11:18:36 +0200

    h

--- src/test/java/Calculator_Test.java
+++ src/test/java/Calculator_Test.java
@@ -53,7 +53,7 @@ public class Calculator_Test {
 
         String calculateinBundleid="com.google.android.calculator";
 
-        driver.activateApp(calculateinBundleid);//burada ise uygulamayi act
+        driver.activateApp(calculateinBundleid);//burada ise uygulamayi actKoc
         Assert.assertTrue( driver.isAppInstalled(calculateinBundleid));
 
 
