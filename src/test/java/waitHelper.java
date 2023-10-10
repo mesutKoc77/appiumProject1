0c1d90a3d4e5cbf4071f35f2c35bed010a14da7c
mesutKoc <mkoc1367@gmail.com>
2023-10-10 11:51:40 +0200

    mesut

---
+++ src/test/java/waitHelper.java
@@ -0,0 +1,12 @@
+public class waitHelper {
+
+    public static void sleep(int seconds) {
+        try {
+            Thread.sleep(seconds*1000); // Saniyeyi milisaniyeye çevirerek bekler
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt(); // Kesinti durumunda hatayı işler
+        }
+    }
+
+
+}
